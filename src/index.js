1a253d21ad4fbeee6dd1154804238835e7f20dfc
Christian Bodensiek <christianbodensiek@gmail.com>
2021-11-12 15:56:12 -0500

    GET user info

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const getUserData= async() =>{
+    try {
+        const user = 'torrenegra';
+        const resp = await fetch(`https://bio.torre.co/api/bios/${ user }`);
+        const data = await resp.json();
+        return data;
+    }catch (error) {
+        console.error(error)
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+getUserData().then( ( { person, strengths }) =>{
+    const name = person.name;
+    document.body.append( name );
+})
+.catch( console.warn );
